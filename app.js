21c25855129a44834ce2bda4d8ace4b85f9b48f1
MizginYildirak <mizgin_yldrak@hotmail.com>
2023-04-18 01:11:57 +0300

    feat: add counter

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+let counter = document.querySelector('.counter');
+let increment = document.querySelector('.increment');
+let decrement = document.querySelector('.decrement')
+
+let count = 0;
+
 // Opening Page
 
 setInterval(function () {
     document.querySelector('.opening').style.display = 'none';
     document.querySelector('.main').style.display = 'block';
 }, 3000)
+
+increment.addEventListener('click', function() {
+    count += 1;
+    counter.innerHTML = count;
+})
+
+decrement.addEventListener('click', function() {
+    count -= 1;
+    counter.innerHTML = count;
+})
