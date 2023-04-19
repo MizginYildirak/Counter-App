ef1f103ee608f2d4bf99379bee7cd51328fd32e1
MİZGİN YILDIRAK <mizgin@MIZGIN-MacBook-Air.local>
2023-04-19 21:32:59 +0300

    add settings toggle

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//variables
+
 let counter = document.querySelector(".counter");
 let increment = document.querySelector(".increment");
 let decrement = document.querySelector(".decrement");
@@ -34,6 +36,7 @@ decrement.addEventListener("click", function () {
 });
 
 function toggleMenu(e) {
+  setVisibilityOfPageTop()
   e.classList.toggle("active");
   document.querySelector(".resetCounter").classList.toggle("active");
   document.querySelector(".reset-hidden").style.display = "block";
@@ -41,6 +44,7 @@ function toggleMenu(e) {
 
 
 function toggleMenuSection(e) {
+  setVisibilityOfPageTop()
   e.classList.toggle("styles-active");
   document.querySelector(".settings").classList.toggle("styles-active");
   document.querySelector(".settings-hidden").style.display = "block";
@@ -56,7 +60,45 @@ function closeScreen() {
 
 function cancelScreen() {
   document.querySelector(".reset-hidden").style.display = "none";
+  document.querySelector(".settings-hidden").style.display = "none"; //buraya bak
 }
 
+function setVisibilityOfPageTop() {
+  document.querySelector(".settings-hidden").style.display = "none";
+  document.querySelector(".reset-hidden").style.display = "none";
+}
 
+// Color change
 
+function handleThemeBtn() {
+  if (event.target.matches('.black-btn')) {
+    document.body.style.backgroundColor = "#141516"
+  } else if (event.target.matches('.white-btn')) {
+    document.body.style.backgroundColor = "#ced4e2"
+  } else if (event.target.matches('.blue-btn')) {
+    document.body.style.backgroundColor = "#004291"
+  } else if (event.target.matches('.pink-btn')) {
+    document.body.style.backgroundColor = "#ed1e78"
+  } else if (event.target.matches('.gray-btn')) {
+    document.body.style.backgroundColor = "#607d8b"
+  } else if (event.target.matches('.purple-btn')) {
+    document.body.style.backgroundColor = "#a701a6"
+  } else if (event.target.matches('.green-btn')) {
+    document.body.style.backgroundColor = "#01ad98"
+  } else if (event.target.matches('.yellow-btn')) {
+    document.body.style.backgroundColor = "#ffac04"
+  }
+}
+
+// const colorButtons = {
+//   "black-btn": "#141516",
+//   "white-btn": "#ced4e2",
+//   "blue-btn": "#004291",
+//   "pink-btn": "#ed1e78",
+//   "gray-btn": "#607d8b",
+//   "purple-btn": "#a701a6",
+//   "green-btn": "#01ad98",
+//   "yellow-btn": "#ffac04",
+// }
+
+// colorButtons["yellow.btn"] 
