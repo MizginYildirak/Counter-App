6a7d960a6adf8d646e753751a0acf0e5115c53f4
MİZGİN YILDIRAK <mizgin@MIZGIN-MacBook-Air.local>
2023-04-21 19:48:55 +0300

    change color theme background

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ let reset = document.querySelector(".reset");
 let toggleSwitch = document.querySelector(".switch-toggle");
 let maximum = document.querySelector(".maximum");
 let availableNumber = document.querySelector(".available-number")
+const btnBorder = document.getElementsByClassName("btn-border");
+
+
 
 console.log(availableNumber)
 let count = 0;
 let currentNumber = 0;
 checkBtn();
+
+
 
 // Opening Page
 
@@ -42,10 +47,6 @@ increment.addEventListener("click", function () {
   checkBtn();
   currentNumber--
   availableNumber.innerHTML = currentNumber;
-
-  if (currentNumber < 0) {
-    availableNumber.style.color = "red"
-  }
 });
 
 
@@ -78,17 +79,28 @@ function toggleMenuSection(e) {
 }
 
 
+document.getElementById("maximum-inner").style.pointerEvents = "none";
+document.getElementById("maximum-inner").style.opacity = "0.2";
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   var checkbox = document.querySelector('.switch-toggle input[type="checkbox"]');
 
   checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
       // do this
-      document.getElementById("maximus-inner").enabled = true;
+      document.getElementById("maximum-inner").style.pointerEvents = "auto";
+      document.getElementById("maximum-inner").style.opacity = "1";
+      document.querySelector(".restrictive").style.visibility = "visible"
     } else {
       // do that
       document.getElementById("maximum-inner").style.pointerEvents = "none";
       document.getElementById("maximum-inner").style.opacity = "0.2";
+      document.querySelector(".restrictive").style.visibility = "hidden"
+      currentNumber = 0;
+      availableNumber.innerHTML = currentNumber;
+      availableNumber.style.color = "white";
     }
   });
 });
@@ -117,48 +129,89 @@ function setVisibilityOfPageTop() {
 
 function changeStyle(color) {
   document.querySelectorAll(".symbol").forEach((item) => {
-       item.style.borderColor = color;
-   });
+    item.style.borderColor = color;
+  })
+
+
 }
+
 
 function handleStyleColorChange() {
   if (event.target.matches('.white-btn')) {
-     document.body.style.color = "#141516"
-     changeStyle("#141516");
-   } else {
-     document.body.style.color = "#ced4e2"
-     changeStyle("#ced4e2");
-   }
+    document.body.style.color = "#141516"
+    changeStyle("#141516");
+  } else {
+    document.body.style.color = "#ced4e2"
+    changeStyle("#ced4e2");
+  }
 }
 
 function handleThemeBtn() {
   if (event.target.matches('.black-btn')) {
+    document.querySelector('.settings').style.backgroundColor = "#141516"
     document.body.style.backgroundColor = "#141516"
+
     handleStyleColorChange()
+
   } else if (event.target.matches('.white-btn')) {
     document.body.style.backgroundColor = "#ced4e2"
     handleStyleColorChange()
-    document.querySelector(".settings-inner").style.color = "#ced4e2"
-    document.querySelector(".reset-hidden").style.color = "#ced4e2"
+    document.querySelector(".settings-inner").style.color = "#141516"
+    document.querySelector('.settings').style.backgroundColor = "#ced4e2"
+
+
   } else if (event.target.matches('.blue-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#004291"
     document.body.style.backgroundColor = "#004291"
+    document.querySelector('.resetCounter').style.backgroundColor = "#004291"
   } else if (event.target.matches('.pink-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#ed1e78"
     document.body.style.backgroundColor = "#ed1e78"
+    document.querySelector('.resetCounter').style.backgroundColor = "#ed1e78"
   } else if (event.target.matches('.gray-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#607d8b"
     document.body.style.backgroundColor = "#607d8b"
+    document.querySelector('.resetCounter').style.backgroundColor = "#607d8b"
   } else if (event.target.matches('.purple-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#a701a6"
     document.body.style.backgroundColor = "#a701a6"
+    document.querySelector('.resetCounter').style.backgroundColor = "#a701a6"
   } else if (event.target.matches('.green-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#01ad98"
     document.body.style.backgroundColor = "#01ad98"
+    document.querySelector('.resetCounter').style.backgroundColor = "#01ad98"
   } else if (event.target.matches('.yellow-btn')) {
     handleStyleColorChange()
+    document.querySelector('.settings').style.backgroundColor = "#ffac04"
     document.body.style.backgroundColor = "#ffac04"
+    document.querySelector('.resetCounter').style.backgroundColor = "#ffac04"
   }
+}
+
+// Add event listener to each button
+for (let i = 0; i < btnBorder.length; i++) {
+  btnBorder[i].addEventListener("click", function () {
+
+    for (let j = 0; j < btnBorder.length; j++) {
+      if (btnBorder[j] !== this) {
+        btnBorder[j].style.border = "none";
+      }
+    }
+    // Change border style of clicked button
+    this.style.border = "2px solid #fff";
+
+    if (event.target.matches('.white-btn')) {
+      document.querySelector(".settings").style.color = "#141516"
+    } else if (!event.target.matches('.white-btn')) {
+      document.querySelector(".settings").style.color = "#ced4e2"
+    }
+  });
+
 }
 
 
